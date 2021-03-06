330ed6e0cc75c48335f3ea91ceefdfdfc7b976fa
Ole Engelbrecht <ole_e@web.de>
2021-03-07 00:36:14 +0100

    update

--- express-server/server.js
+++ express-server/server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', function(req, res){
-    res.send("hello world!")
+    res.send("Hello world!")
 });
 
 app.listen(port, () => {
